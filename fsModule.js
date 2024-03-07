544889f0980476a9dbefb675c6c42003db688def
Rahul Devarapalli <rahuldevarapalli02@gmail.com>
2024-03-07 07:38:57 +0530

    change code readFile to writeFile

--- fsModule.js
+++ fsModule.js
@@ -3,13 +3,13 @@
 const fs = require("fs");
 console.log("im working fine")
 
-// fs.writeFile('message.txt', 'Hello Node.js', 'utf8',(err)=>{
-//     if(err) throw err;
-//     console.log('message saved in file successfully');
-// });
-
-
-fs.readFile('./message.txt','utf-8',(err,data)=>{
+fs.writeFile('message.txt', 'Hello Node.js', 'utf8',(err)=>{
     if(err) throw err;
-    console.log(data);
-})
+    console.log('message saved in file successfully');
+});
+
+
+// fs.readFile('./message.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
